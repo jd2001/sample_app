0aa2811e609da9743bd7748e95fd3af38867b27e
jd2001 <joshuadeppe15@gmail.com>
2019-06-27 14:19:48 +0000

    Added logout functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,11 @@ class User < ApplicationRecord
   
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remeber_digest.nil
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
